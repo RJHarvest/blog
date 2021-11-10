813600021f3da18b51025208baa910faaf1770c5
Russell Gapasin <russell@fanswifi.com>
2021-11-10 20:45:19 +0800

    Added graphql route to api pages

---
+++ pages/api/graphql.js
@@ -0,0 +1,36 @@
+import { ApolloServer } from 'apollo-server-micro'
+import typeDefs from '../../db/data/schema'
+import resolvers from '../../db/data/resolvers'
+import connectDb from '../../db/config'
+
+connectDb()
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const startServer = apolloServer.start()
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://studio.apollographql.com'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
