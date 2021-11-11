be41ea72c821db12f24e5304a6018d217439c4e8
Russell Gapasin <russell@fanswifi.com>
2021-11-11 23:56:08 +0800

    Added session provider layer

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
+import { Provider } from 'next-auth/client'
 import Layout from '../components'
 import 'tailwindcss/tailwind.css'
 
 const categories = ['food', 'travel', 'tech', 'lifestyle']
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout header={pageProps.header}>
-      <Component {...pageProps} categories={categories} />
-    </Layout>
+    <Provider session={session}>
+      <Layout header={pageProps.header}>
+        <Component {...pageProps} categories={categories} />
+      </Layout>
+    </Provider>
   )
 }
 
