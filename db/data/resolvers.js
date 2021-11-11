6e175e720604e08e1264c8a91b3c74d61157c906
Russell Gapasin <russell@fanswifi.com>
2021-11-11 23:53:22 +0800

    Added authentication and user input error

--- db/data/resolvers.js
+++ db/data/resolvers.js
@@ -1,14 +1,11 @@
+import { AuthenticationError, UserInputError } from 'apollo-server-micro'
 import Blog from '../models/blogs'
 
 const resolvers = {
   Query: {
     blogs: async (_, { limit, page, type }) => {
-      try {
-        const query = type ? { type } : {}
-        return await Blog.paginate(query, { limit, page, sort: { createdAt: 'desc' } })
-      } catch (err) {
-        throw err
-      }
+      const query = type ? { type } : {}
+      return await Blog.paginate(query, { limit, page, sort: { createdAt: 'desc' } })
     },
     blog: async (_, { id }) => {
       const blog = await Blog.findById(id)
@@ -32,34 +29,41 @@ const resolvers = {
 
   Mutation: {
     // blogs
-    newBlog: async (_, { input }) => {
+    newBlog: async (_, { input }, { session }) => {
+      if (!session) throw new AuthenticationError('User must be logged in')
       try {
         const blog = new Blog(input)
         return await blog.save()
       } catch (err) {
-        console.log(err)
+        throw err
       }
     },
-    updateBlog: async (_, { id, input }) => {
+    updateBlog: async (_, { id, input }, { session }) => {
+      if (!session) throw new AuthenticationError('User must be logged in')
+
       const blog = await Blog.findById(id)
-      if (!blog) throw new Error('Blog not found')
+      if (!blog) throw new UserInputError('Blog not found')
 
       return await Blog.findOneAndUpdate({ _id: id }, input, {
         new: true,
       })
     },
     // comments
-    newComment: async (_, { blogId, input }) => {
+    newComment: async (_, { blogId, input }, { session }) => {
+      if (!session) throw new AuthenticationError('User must be logged in')
+
       const blog = await Blog.findById(blogId)
-      if (!blog) throw new Error('Blog not found')
+      if (!blog) throw new UserInputError('Blog not found')
 
       blog.comments.push(input)
       blog.markModified('comments')
       return await blog.save()
     },
-    updateComment: async (_, { blogId, commentId, input }) => {
+    updateComment: async (_, { blogId, commentId, input }, { session }) => {
+      if (!session) throw new AuthenticationError('User must be logged in')
+
       const blog = await Blog.findById(blogId)
-      if (!blog) throw new Error('Blog not found')
+      if (!blog) throw new UserInputError('Blog not found')
 
       blog.comments.id(commentId).comment = input.comment
       blog.markModified('comments')
