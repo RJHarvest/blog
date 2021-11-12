92d9308b8313f30c4589886eab0f84b972c6a68f
Russell Gapasin <russell@fanswifi.com>
2021-11-13 05:11:37 +0800

    Changed hardcode user data to real data

--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -51,15 +51,15 @@ export default function BlogDetails({ props }) {
         <div className="w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm">
           <div className="p-4 border-t border-b border-gray-400 md:border md:rounded">
             <div className="flex py-2">
-              <img src="https://randomuser.me/api/portraits/men/97.jpg"
+              <img src={blog.user.imageUrl}
                 className="h-10 w-10 rounded-full mr-2 object-cover" />
               <div>
-                <p className="font-semibold text-gray-700 text-sm">Mike Sullivan</p>
+                <p className="font-semibold text-gray-700 text-sm">{blog.user.name}</p>
                 <p className="font-semibold text-gray-600 text-xs">Editor</p>
               </div>
             </div>
             <p className="text-gray-700 py-3">
-              Mike writes about technology
+              Jeff writes about technology
               Yourself required no at thoughts delicate landlord it be. Branched dashwood do is whatever it.
             </p>
             <button className="px-2 py-1 text-gray-100 bg-green-700 flex w-full items-center justify-center rounded">
