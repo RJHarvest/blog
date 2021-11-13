b81a8f02d8b123a36ed7c824dd5101443df8ffb1
Russell Gapasin <russell@fanswifi.com>
2021-11-14 01:34:27 +0800

    Fixed blog form

--- components/Form/Blog.js
+++ components/Form/Blog.js
@@ -1,50 +1,29 @@
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { getBlogs, insertBlog } from '../../db/data/query'
-import Alert from '../Alert'
 
-export default function BlogForm({ setBlogs }) {
+export default function BlogForm({ blog, onSubmit }) {
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm()
-
-  const [showAlert, setShowAlert] = useState(false)
-  const [errorMsg, setErrorMsg] = useState('')
-
-  const onSubmit = async (data, e) => {
-    e.preventDefault()
-
-    try {
-      await insertBlog(data)
-      const newBlogs = await getBlogs()
-      setBlogs(newBlogs)
-      e.target.reset()
-    } catch (err) {
-      setErrorMsg(`${err.status}: ${err.statusText}`)
-      setShowAlert(true)
-    }
-  }
+  } = useForm({ defaultValues: blog ? blog : null })
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
-      <Alert errorMsg={errorMsg} showAlert={showAlert} setShowAlert={setShowAlert} />
       <div className='mb-4'>
         <label className="block text-left text-gray-700 text-sm font-bold mb-2" htmlFor="title">Title</label>
         <input type='text' {...register('title', { required: true })} id='title' placeholder='title' className={`block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.title ? 'border-red-500': null}`} />
-        {errors.title && <p className="text-red-500 text-xs text-left italic">Please enter a title</p>}
+        {errors.title && <p className="mt-1 text-red-500 text-xs text-left italic">Please enter a title</p>}
       </div>
       <div className='mb-4'>
         <label className="block text-left text-gray-700 text-sm font-bold mb-2" htmlFor="body">Blog Post</label>
         <textarea
           {...register('body', { required: true })}
           id='body'
-          rows='4'
+          rows='6'
           placeholder='Enter your blog post here...'
           className={`block shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${errors.body ? 'border-red-500': null}`}
         ></textarea>
-        {errors.body && <p className="text-red-500 text-xs text-left italic">Please enter a blog post.</p>}
+        {errors.body && <p className="mt-1 text-red-500 text-xs text-left italic">Please enter a blog post.</p>}
       </div>
       <div className='mb-4'>
         <label className="block text-left text-gray-700 text-sm font-bold mb-2" htmlFor="type">Type</label>
@@ -55,9 +34,9 @@ export default function BlogForm({ setBlogs }) {
           <option value='tech'>Tech</option>
           <option value='lifestyle'>Lifestyle</option>
         </select>
-        {errors.type && <p className="text-red-500 text-xs text-left italic">Please select a blog type.</p>}
+        {errors.type && <p className="mt-1 text-red-500 text-xs text-left italic">Please select a blog type.</p>}
       </div>
-      <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Post</button>
+      <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>{blog ? 'Save' : 'Create'}</button>
     </form>
   )
 }
